535e377b6a83bb332ae6a1db13b6d61cd24dcf5b
Manjodh 'Mango' Singh <manjodheriksingh@gmail.com>
2020-02-16 12:31:32 +0100

    Added UTs for calc-average-color
    Took 22 minutes

--- src/calc-average-color.js
+++ src/calc-average-color.js
@@ -3,6 +3,18 @@
 export const calcAverageColor = (colorBlock: Array<{ r: number, g: number, b: number }>): { r: number, g: number, b: number } => {
   const totalPixels = colorBlock.length
   
+  if(typeof colorBlock !== 'object' || typeof colorBlock.reduce === 'undefined') {
+    throw new Error('calcAverageColor: only accepts array of colors')
+  }
+  
+  if(totalPixels === 0) {
+    throw new Error('calcAverageColor: 0 pixels found')
+  }
+  
+  if(totalPixels === 1) {
+    return colorBlock[0]
+  }
+  
   const rgbAverage = colorBlock.reduce((rgbAcc, colorsObj) => {
     rgbAcc[0] += Math.round(colorsObj.r / totalPixels)
     rgbAcc[1] += Math.round(colorsObj.g / totalPixels)
---
+++ tests/calc-average-color.test.js
@@ -0,0 +1,41 @@
+import { calcAverageColor } from '../src/calc-average-color'
+
+const black = {
+  r: 0,
+  g: 0,
+  b: 0
+}
+const white = {
+  r: 255,
+  g: 255,
+  b: 255
+}
+
+describe('calcAverageColor should', () => {
+  const mockColorBlock = [
+    black,
+    white
+  ]
+  
+  it('return the average color', () => {
+    expect(calcAverageColor(mockColorBlock)).toEqual({
+      r: 128,
+      g: 128,
+      b: 128
+    })
+  })
+  
+  it('return one color if color block only has one pixel', () => {
+    expect(calcAverageColor([black])).toEqual(black)
+  })
+  
+  it('throw if 0 pixels in color block', () => {
+    expect(() => calcAverageColor([])).toThrow('calcAverageColor: 0 pixels found')
+  })
+  
+  it('throw if argument is not array', () => {
+    expect(() => calcAverageColor(black)).toThrow('calcAverageColor: only accepts array of colors')
+    expect(() => calcAverageColor('black')).toThrow('calcAverageColor: only accepts array of colors')
+    expect(() => calcAverageColor(35)).toThrow('calcAverageColor: only accepts array of colors')
+  })
+})
